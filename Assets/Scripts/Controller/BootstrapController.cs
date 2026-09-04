78f0cacb206e90d3dd0a0c9e565d3fd1d7625b33
Howby <anggerpdsw@gmail.com>
2026-09-04 16:01:27 +0700

    Minor

--- Assets/Scripts/Controller/BootstrapController.cs
+++ Assets/Scripts/Controller/BootstrapController.cs
@@ -21,7 +21,7 @@ namespace IdleDefenseSurvival.Core
     public class BootstrapController : MonoBehaviour
     {
         public static bool IsInitialized { get; private set; }
-        private const string MainMenuSceneName = "Main"; // production = "MainMenu"
+        private const string MainMenuSceneName = "MainMenu"; // production = "MainMenu"
         private void Awake()
         {
             // Ensure this GameObject persists across scene loads.
